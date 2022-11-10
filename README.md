8b70f4cbd613d34b275f8d64df68c176b57d9c54
zxp-yuluo <zxp_10012991@163.com>
2022-11-10 17:23:57 +0800

    修改readme中的图片地址，删除counter.js中的有关redux的API

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
  我们建议使用@reduxjs/toolkit包的configureStore方法，它取代了createStore。
 ```
 
-![](src\img\redux.png)
+![](src/img/redux.png)
 
 ## store.js
 
@@ -409,5 +409,23 @@ export const incrementAction = (data) => ({type: INCREMENT,data})
 export const decrementAction = (data) => ({type: DCREMENT,data})
 ```
 
+# react-redux
 
+> 一个官方维护的react插件库。
+>
+> 用于简化react应用中使用redux。
 
+## react-redux 将所有的组件分为两大类
+
+### UI组件
+
+- 只负责UI的呈现，不带任何业务逻辑。
+- 通过props接收数据(一般数据和函数)。
+- 不适用任何Redux的API。
+- 一般保存在components文件夹下。
+
+### 容器组件
+
+- 负责管理数据和业务逻辑，不负责UI呈现。
+- 使用 Redux 的API。
+- 一般保存在containers文件夹下。
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,39 +1,32 @@
 import { useRef } from "react"
-import { incrementAction, decrementAction } from "../redux/action_creators"
 const Counter = (props) => {
   const selectValue = useRef()
-  const { store } = props
-  const count = store.getState()
   // 加
   const increment = () => {
     const value = selectValue.current.value * 1
-    // 分发action
-    store.dispatch(incrementAction(value))
   }
 
   // 减
   const decrement = () => {
     const value = selectValue.current.value * 1
-    store.dispatch(decrementAction(value))
   }
 
   // 如果是奇数就加
   const oddIncrement = () => {
     const value = selectValue.current.value * 1
-    if (count % 2 === 0) return
-    store.dispatch(incrementAction(value))
+    // if (count % 2 === 0) return
   }
 
   // 两秒后加
   const asyncIncrement = () => {
     const value = selectValue.current.value * 1
     setTimeout(() => {
-      store.dispatch(incrementAction(value))
+      
     }, 2000)
   }
   return (
     <div className="App">
-      <div>count：{count}</div>
+      <div>count：100</div>
       <select ref={selectValue}>
         <option value="1">1</option>
         <option value="2">2</option>
